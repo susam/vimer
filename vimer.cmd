6e445fb0be6207788f27413ae707d0974e8fc305
Konfekt <Konfekt@users.noreply.github.com>
2024-04-09 20:32:40 +0200

    condense batch file

--- vimer.cmd
+++ vimer.cmd
@@ -1,4 +1,5 @@
-@echo off & goto :main
+@echo off
+goto :main
 
 rem Vimer - A convenience wrapper for gvim --remote(-tab)-silent.
 
@@ -25,8 +26,6 @@ rem CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 rem TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 rem SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
-rem Starting point of this script.
 :main
     setlocal
     set VERSION=0.2.1-DEV
@@ -46,74 +45,56 @@ rem Starting point of this script.
     endlocal
     goto :eof
 
-
-    rem Automatically detect the path of GVim executable.
-    rem
-    rem If GVim is found on the system, set VIM_CMD to its path; do not set
-    rem VIM_CMD otherwise.
-    :find_vim
-    rem Automatically detect the path of GVim executable.
+:find_vim
+    rem Check registry for GVim path.
     reg query HKLM\SOFTWARE\Vim\GVim > nul 2>&1
     IF %ERRORLEVEL% EQU 0 (
-      for /f "tokens=2*" %%a in (
-      'reg query HKLM\SOFTWARE\Vim\GVim') do set VIM_CMD="%%b"
-      IF EXIST "%VIM_CMD%" (
-        goto :eof
-      )
+        for /f "tokens=2*" %%a in ('reg query HKLM\SOFTWARE\Vim\GVim') do set VIM_CMD="%%b"
+        IF EXIST "%VIM_CMD%" goto :eof
     )
-    rem first try exe ...
+    rem Check for gvim.exe in system path.
     where /q gvim.exe
     IF NOT ERRORLEVEL 1 (
-      SET VIM_CMD=gvim.exe
-      goto :eof
-    )
-    rem ... finally try %EDITOR%
-    IF DEFINED EDITOR (
-      IF EXIST "%EDITOR%" (
-        SET VIM_CMD=%EDITOR%
+        SET VIM_CMD=gvim.exe
         goto :eof
-      ) ELSE (
-        where /q %EDITOR%
-        IF NOT ERRORLEVEL 1 (
-          SET VIM_CMD=%EDITOR%
-          goto :eof
+    )
+    rem Check for %EDITOR% environment variable.
+    IF DEFINED EDITOR (
+        IF EXIST "%EDITOR%" (
+            SET VIM_CMD=%EDITOR%
+            goto :eof
+        ) ELSE (
+            where /q %EDITOR%
+            IF NOT ERRORLEVEL 1 (
+                SET VIM_CMD=%EDITOR%
+                goto :eof
+            )
         )
-      )
     )
     goto :eof
 
-rem Parse command line arguments passed to this script.
-rem
-rem Arguments:
-rem   arg...: All arguments this script was invoked with
 :parse_arguments
-    rem Parse options.
+    rem Parse options and arguments.
     if "%~1" == "-t" (
         set tab=-tab
         shift
-        goto :parse_arguments
     ) else if "%~1" == "--tab" (
         set tab=-tab
         shift
-        goto :parse_arguments
     ) else if "%~1" == "-s" (
         if "%~2" == "" (
             call :err Argument missing after: "%~1".
             exit /b 1
         )
         set server=%~2
-        shift
-        shift
-        goto :parse_arguments
+        shift & shift
     ) else if "%~1" == "--server" (
         if "%~2" == "" (
             call :err Argument missing after: "%~1".
             exit /b 1
         )
         set server=%~2
-        shift
-        shift
-        goto :parse_arguments
+        shift & shift
     ) else if "%~1" == "-n" (
         call :show_name
         goto :eof
@@ -155,14 +136,12 @@ rem   arg...: All arguments this script was invoked with
         goto :eof
     )
 
+    rem Start GVim if no file arguments are specified.
     if "%~1" == "" (
-        rem If no file arguments are specified, start a new instance of
-        rem GVim.
         call :exec_vim
         goto :eof
     ) else if "%~1" == "-" (
-        rem If hyphen is specified as a file argument, read input from
-        rem standard input.
+        rem Read input from standard input if hyphen is specified.
         if not "%~2" == "" (
             call :err Too many edit arguments: "%~2".
             exit /b 1
@@ -171,135 +150,76 @@ rem   arg...: All arguments this script was invoked with
         goto :eof
     )
 
-    rem Consume remaining file arguments.
+    rem Process remaining file arguments.
+    set args=
     :consume_args
+        if "%~1" == "" goto :exec_vim
         set args=%args% "%~1"
         shift
-        if not "%~1" == "" goto :consume_args
+        goto :consume_args
 
-    rem Execute GVim with the remaining file arguments.
-    call :exec_vim %args%
-    goto :eof
-
-
-rem Save standard input in a temporary file and edit it.
 :exec_vim_with_stdin
     setlocal
-
-    rem Create temporary directory.
     set tmp_dir=%temp%\vimer
-    if not exist %tmp_dir% (
-        mkdir %tmp_dir%
-        if errorlevel 1 (
-            call :err Cannot create temporary directory: %tmp_dir%.
-            exit /b 1
-        )
-    )
-
-    rem Determine a unique temporary filename.
+    if not exist %tmp_dir% mkdir %tmp_dir%
     set filename=_STDIN_%random%_%date:/=-%_%time::=-%.tmp
     set filename=%filename: =_%
     set stdin_file="%tmp_dir%\%filename%"
-    if exist "%stdin_file%" (
-        endlocal
-        goto :exec_vim_with_stdin
-    )
-
-    rem Create temporary file and edit it.
+    if exist "%stdin_file%" goto :exec_vim_with_stdin
     findstr "^" > "%stdin_file%"
     call :exec_vim "%stdin_file%"
-
     endlocal
     goto :eof
 
-
-rem Execute GVim with specified arguments.
-rem
-rem Arguments:
-rem   arg...: One or more arguments to GVim
-rem
-rem Errors:
-rem   Exit with an error message if VIM_CMD variable is not set.
 :exec_vim
     if "%VIM_CMD%" == "" (
         call :err Cannot find GVim.
         exit /b 1
     )
-
-    rem If server name is specified, open files in an existing instance
-    rem of GVim with the specified server name, or start a new instance
-    rem of GVim with the specified server name.
-    if not "%server%" == "" (
-        set opts=%opts% --servername "%server%"
-    )
-
-    rem If file arguments are specified, open them in an existing
-    rem instance of GVim.
-    if not "%~1" == "" (
-        set opts=%opts% --remote%tab%-silent
-    )
-
-    rem gvim instance already running?
+    if not "%server%" == "" set opts=%opts% --servername "%server%"
+    if not "%~1" == "" set opts=%opts% --remote%tab%-silent
     QPROCESS gvim.exe >NUL
     IF %ERRORLEVEL% EQU 0 (
-      rem If file arguments are specified, open them in an existing
-      rem instance of GVim.
-      if EXIST "%~f1" (
-        start /b "" %VIM_CMD% %opts% --remote%tab%-silent %*
-      )
+        if EXIST "%~f1" start /b "" %VIM_CMD% %opts% --remote%tab%-silent %*
     ) else (
-      start /b "" %VIM_CMD% %opts% %*
+        start /b "" %VIM_CMD% %opts% %*
     )
-
     goto :eof
 
-
-rem Show the known name or path of GVim executable.
 :show_name
     echo %VIM_CMD%
     call :pause
     goto :eof
 
-
-rem Add a new context menu option to open files with this script.
 :enable
     setlocal
-    if "%tab%" == "-tab" (set k=%TAB_SHORTCUT%) else (set k=%BUF_SHORTCUT%)
+    set k=%BUF_SHORTCUT%
+    if "%tab%" == "-tab" set k=%TAB_SHORTCUT%
     set d=\"%VIM_CMD%\" --remote%tab%-silent \"%%1\"
     reg add "HKCR\*\shell\%k%\command" /f /ve /d "%d%"
     endlocal
     call :pause
     goto :eof
 
-
-rem Remove context menu option to open files with this script.
 :disable
     setlocal
-    if "%tab%" == "-tab" (set k=%TAB_SHORTCUT%) else (set k=%BUF_SHORTCUT%)
+    set k=%BUF_SHORTCUT%
+    if "%tab%" == "-tab" set k=%TAB_SHORTCUT%
     reg delete "HKCR\*\shell\%k%" /f
     endlocal
     call :pause
     goto :eof
 
-
-rem Show the path of this script.
 :where_am_i
     echo %~f0
     call :pause
     goto :eof
 
-
-rem Print error message.
-rem
-rem Arguments:
-rem   string...: String to print to standard error stream.
 :err
     >&2 echo %NAME%: %*
     call :pause
     goto :eof
 
-
-rem Show help.
 :show_help
     echo Usage: %NAME% [-t] [-s] [-e^|-d] [-n] [-w] [-h] [-v] [-^|FILE...]
     echo.
@@ -326,12 +246,10 @@ rem Show help.
     echo   -v, --version      Show version and exit.
     echo.
     echo Report bugs to ^<%SUPPORT_URL%^>.
-call :pause
-goto :eof
+    call :pause
+    goto :eof
 
-
-rem Show version and copyright.
-    :show_version
+:show_version
     echo Vimer %VERSION%
     echo %COPYRIGHT%
     echo.
@@ -345,8 +263,6 @@ rem Show version and copyright.
     call :pause
     goto :eof
 
-
-rem Pause if this script was invoked from command prompt.
 :pause
     echo %cmdcmdline% | findstr /i /c:"%~nx0" > nul && pause > nul
     goto :eof
